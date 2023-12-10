36509fde0bb6a468a6a9de7bc940f616d05272d1
Nurgüzel Keles <keles.nu01@gmail.com>
2023-12-10 14:19:37 +0100

    added method to print the board

--- src/main/java/org/example/Board.java
+++ src/main/java/org/example/Board.java
@@ -5,4 +5,13 @@ public class Board {
     public Board() {
         cells = new char[3][3];
     }
+    public void print() {
+        System.out.println();
+        System.out.println(" " + cells[0][0] + " | " + cells[0][1] + " | " + cells[0][2]);
+        System.out.println("---+---+---");
+        System.out.println(" " + cells[1][0] + " | " + cells[1][1] + " | " + cells[1][2]);
+        System.out.println("---+---+---");
+        System.out.println(" " + cells[2][0] + " | " + cells[2][1] + " | " + cells[2][2]);
+        System.out.println();
+    }
 }
