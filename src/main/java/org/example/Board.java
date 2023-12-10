905ac0784b06253a7811c1a267b0b2ea785d441a
Nurgüzel Keles <keles.nu01@gmail.com>
2023-12-10 11:29:21 +0100

    Initial Commit - Board Class

---
+++ src/main/java/org/example/Board.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Board {
+    protected char[][] cells; // zweidimensionale Array von Zeichen, das das Spielfeld darstellt
+    public Board() {
+        cells = new char[3][3];
+    }
+}
