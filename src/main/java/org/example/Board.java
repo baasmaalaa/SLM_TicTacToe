5a3d23c96628952efb92aa4536efa211eec73c90
Nurgüzel Keles <keles.nu01@gmail.com>
2023-12-10 14:38:25 +0100

    added method to clear the board

--- src/main/java/org/example/Board.java
+++ src/main/java/org/example/Board.java
@@ -4,6 +4,7 @@ public class Board {
     protected char[][] cells; // zweidimensionale Array von Zeichen, das das Spielfeld darstellt
     public Board() {
         cells = new char[3][3];
+        clear();
     }
     public void print() {
         System.out.println();
@@ -13,5 +14,14 @@ public class Board {
         System.out.println("---+---+---");
         System.out.println(" " + cells[2][0] + " | " + cells[2][1] + " | " + cells[2][2]);
         System.out.println();
+    }
+    public void clear() {
+        // Durchlaufe alle Zellen des Spielfelds
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                // Setze jede Zelle auf ein Leerzeichen
+                cells[i][j] = ' ';
+            }
+        }
     }
 }
