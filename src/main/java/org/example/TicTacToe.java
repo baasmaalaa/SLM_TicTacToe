6b1cd3f752e3116e214d349cdb5bd41370cb10ef
Nurgüzel Keles <keles.nu01@gmail.com>
2023-12-10 14:11:05 +0100

    Initial commit - TicTacToe class with basic structure and player initialization

---
+++ src/main/java/org/example/TicTacToe.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class TicTacToe {
+
+    private Player player1;
+    private Player player2;
+    private Player currentPlayer;
+    private Board board;
+    public TicTacToe() {
+        player1 = new Player('X');
+        player2 = new Player('O');
+        currentPlayer = player1;
+        board = new Board();
+    }
+}
