a8792facd627c4968ea78633bf7cc5f8803352bd
baasmaalaa <basmalae60@gmail.com>
2024-01-01 22:23:17 +0100

    add test class for TicTacToe class

---
+++ src/test/java/org/example/TicTacToeTest.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+public class TicTacToeTest extends TestCase {
+
+    @Test
+    public void testStart() {
+    }
+
+    @Test
+    public void testSwitchCurrentPlayer() {
+    }
+    @Test
+    public void testHasWinner() {
+    }
+}
