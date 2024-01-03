d4e9b5d9cea6d0d67398a65c9a65f309355d5a5d
baasmaalaa <basmalae60@gmail.com>
2024-01-03 10:00:05 +0100

    import junit.Assert to PlayerTest and TicTacToeTest

--- src/test/java/org/example/PlayerTest.java
+++ src/test/java/org/example/PlayerTest.java
@@ -2,6 +2,8 @@ package org.example;
 
 
 import org.junit.Test;
+import static org.junit.Assert.*;
+
 
 public class PlayerTest {
 
--- src/test/java/org/example/TicTacToeTest.java
+++ src/test/java/org/example/TicTacToeTest.java
@@ -1,6 +1,8 @@
 package org.example;
 
 import org.junit.Test;
+import static org.junit.Assert.*;
+
 
 public class TicTacToeTest {
 
