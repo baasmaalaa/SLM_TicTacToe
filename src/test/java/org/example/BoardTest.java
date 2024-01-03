3c28da60077071a94f4e07a268e146f998e5116e
baasmaalaa <basmalae60@gmail.com>
2024-01-03 09:56:02 +0100

    add tests for isFull() method

--- src/test/java/org/example/BoardTest.java
+++ src/test/java/org/example/BoardTest.java
@@ -61,6 +61,20 @@ public class BoardTest {
 
     @Test
     public void testIsFull() {
+        Board board = new Board();
+        assertFalse(board.isFull());
+
+        board.place(0,0,'X');
+        board.place(0,1,'O');
+        board.place(0,2,'X');
+        board.place(1,0,'X');
+        board.place(1,1,'O');
+        board.place(1,2,'X');
+        board.place(2,0,'O');
+        board.place(2,1,'X');
+        board.place(2,2,'O');
+        assertTrue(board.isFull());
+
     }
 
     @Test
