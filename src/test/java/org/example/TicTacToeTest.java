7daec69498fb5f4026aec8ae331a6bcefb2d59d6
baasmaalaa <basmalae60@gmail.com>
2024-01-03 11:07:44 +0100

    add tests for hasWinner() method

--- src/test/java/org/example/TicTacToeTest.java
+++ src/test/java/org/example/TicTacToeTest.java
@@ -22,5 +22,28 @@ public class TicTacToeTest {
     }
     @Test
     public void testHasWinner() {
+        TicTacToe game = new TicTacToe();
+        game.getBoard().place(0,0,'X');
+        game.getBoard().place(0,1,'O');
+        game.getBoard().place(0,2,'X');
+        game.getBoard().place(1,0,'X');
+        game.getBoard().place(1,1,'O');
+        game.getBoard().place(1,2,'X');
+        game.getBoard().place(2,0,'O');
+        game.getBoard().place(2,1,'X');
+        game.getBoard().place(2,2,'O');
+        assertFalse(game.hasWinner());
+
+        game.getBoard().clear();
+        game.getBoard().place(0,0,'X');
+        game.getBoard().place(0,1,'O');
+        game.getBoard().place(0,2,'X');
+        game.getBoard().place(1,0,'X');
+        game.getBoard().place(1,1,'O');
+        game.getBoard().place(1,2,'O');
+        game.getBoard().place(2,0,'X');
+        game.getBoard().place(2,1,'X');
+        game.getBoard().place(2,2,'O');
+        assertTrue(game.hasWinner());
     }
 }
