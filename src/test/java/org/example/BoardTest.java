efcf25c2567f97985e64d9a1b52d5ecd0aea2688
baasmaalaa <basmalae60@gmail.com>
2024-01-03 09:56:18 +0100

    add test for clear() method

--- src/test/java/org/example/BoardTest.java
+++ src/test/java/org/example/BoardTest.java
@@ -79,5 +79,21 @@ public class BoardTest {
 
     @Test
     public void testClear() {
+        Board board = new Board();
+
+        board.place(0,0,'X');
+        board.place(0,1,'O');
+        board.place(0,2,'X');
+        board.place(1,0,'X');
+        board.place(1,1,'O');
+        board.place(1,2,'X');
+        board.place(2,0,'O');
+        board.place(2,1,'X');
+        board.place(2,2,'O');
+        board.clear(); // wüsste nicht, wie man das noch testen kann
+        assertFalse(board.isFull());
+
+
+
     }
 }
